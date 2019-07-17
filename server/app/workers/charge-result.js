d12d43e88a18d2a7101cbac6138bc6d7901a2617
bentesha <benedict.tesha@jamaatech.com>
2019-07-17 21:53:02 +0300

    fix: fail payment session if charge request fails

--- server/app/workers/charge-result.js
+++ server/app/workers/charge-result.js
@@ -2,9 +2,9 @@
 
 const updatePaymentStatus = require('../use-cases/update-payment-status')
 
-module.exports = async function({ data }) {
+module.exports = async function({ data: result }) {
   /** @type {import('..')} */
   const app = this
 
-  await updatePaymentStatus(app, data.reference, true, data.transactionId)
+  await updatePaymentStatus(app, result.reference, result.success, result.transactionId)
 }
